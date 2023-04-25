a1e742f907f59596195cb1ab7ef9e78c6aa3daff
humorW <121972279+humorW@users.noreply.github.com>
2023-04-25 14:10:06 -0700

    restart button working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,16 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+    
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
