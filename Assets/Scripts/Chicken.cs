a0a8aa4aacdc65ff2d2b92ba59893c551e419bc7
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-05-09 14:26:07 -0700

    Fences Finished

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Chicken : MonoBehaviour
 {
     public float speed;
+    private float xBound = 7.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,18 @@ public class Chicken : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+    void RotatePosition()
+    {
+        if (transform.position.x < -xBound)
+        {
+            transform.Rotate = new Vector3(transform.Rotate.x, 90, transform.Rotate.z);
+        }
+
+        if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+        }
     }
 }
