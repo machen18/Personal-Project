dea435e24fb84effea5901edf152e37570acfb77
humorW <121972279+humorW@users.noreply.github.com>
2023-05-12 15:42:58 -0700

    new  speed over time

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+
+
+    public float accelerationRate; 
+    public float maxSpeed; 
+    private float currentSpeed; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+         currentSpeed = 0f;
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.position = player.transform.position + offset;
+        currentSpeed += accelerationRate * Time.deltaTime;
+        currentSpeed = Mathf.Min(currentSpeed, maxSpeed);
+        transform.position += new Vector3(currentSpeed * Time.deltaTime, 0f, 0f);
     }
 }
