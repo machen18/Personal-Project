007bf20f3f8cec676b43b2866a948a3039f0e410
candy67-123 <zizhao4@s.sfusd.edu>
2023-05-15 14:00:30 -0700

    small change of obstacle

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    private float lowerBound = -16;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-}
+    
+       
+
+
+
+ }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
+
     // Start is called before the first frame update
     void Start()
     {
