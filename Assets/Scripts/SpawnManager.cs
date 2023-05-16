5c538dbccad4a4ecc4e8e6f6209d149ca0a85ae6
candy67-123 <zizhao4@s.sfusd.edu>
2023-05-16 13:42:54 -0700

    final update for spawn obstacle

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,23 +8,30 @@ public class SpawnManager : MonoBehaviour
     public GameObject player;
     private float spawnRangeX = 8;
     private float spawnPosZ = 20;
+    private float startDelay = 1;
+    private float spawnInterval = 0.5f;
     private Vector3 offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, Perfabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-            Instantiate(Perfabs[animalIndex],spawnPos, Perfabs[animalIndex].transform.rotation);
-        }
+        
     }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, Perfabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(Perfabs[animalIndex], spawnPos, Perfabs[animalIndex].transform.rotation);
+    }
+
+
+
 }
