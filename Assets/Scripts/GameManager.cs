657e8e3c22edaf7ce6f14b2c0cc72fcd76f95e05
humorW <121972279+humorW@users.noreply.github.com>
2023-05-16 14:30:33 -0700

    make the startgame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,12 +10,21 @@ public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
-
+    public Button startButton; 
+    public GameObject hideObject;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+       startButton.onClick.AddListener(StartGame);
+
+       void StartGame()
+    {
+        Debug.Log("start");
+        hideObject.SetActive(false);
+        
+    }
+
     }
 
     // Update is called once per frame
